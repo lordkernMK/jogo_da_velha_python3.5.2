8f28a053b2b3014a807d40a67442f60e567b8a4e
lordkernMK <lordkernMK@gmail.com>
2016-09-29 10:28:35 -0300

    Jogo da Velha

---
+++ jogo_da_velha.py
@@ -0,0 +1,108 @@
+import os
+    
+#cria matriz 3x3
+vetor1=[' ',' ',' ']
+vetor2=[' ',' ',' ']
+vetor3=[' ',' ',' ']
+matriz=[vetor1,vetor2,vetor3]
+
+#imprime o ambiente na tela
+def ambiente():
+    
+    print ("JOGO DA VELHA")
+    print ("----------------")
+    print ('','1',matriz[0][0],'2',matriz[0][1],'3',matriz[0][2],'',sep=" | ")
+    print ('','4',matriz[1][0],'5',matriz[1][1],'6',matriz[1][2],'',sep=" | ")
+    print ('','7',matriz[2][0],'8',matriz[2][1],'9',matriz[2][2],'',sep=" | ")
+    print ("----------------")
+
+#captura as jogas do player
+def jogadas_humana():
+    
+    jogada_humana = int(input("Jogada Humana = "))
+    if jogada_humana == 1 and matriz[0][0] == ' ':
+        matriz[0][0]= 'X'
+        
+    elif jogada_humana == 2 and matriz[0][1] == ' ':
+        matriz[0][1]= 'X'
+        
+    elif jogada_humana == 3 and matriz[0][2] == ' ':
+        matriz[0][2]= 'X'
+        
+    elif jogada_humana == 4 and matriz[1][0] == ' ':
+        matriz[1][0]= 'X'
+    
+    elif jogada_humana == 5 and matriz[1][1] == ' ':
+        matriz[1][1]= 'X'
+    
+    elif jogada_humana == 6 and matriz[1][2] == ' ':
+        matriz[1][2]= 'X'
+   
+    elif jogada_humana == 7 and matriz[2][0] == ' ':
+        matriz[2][0]= 'X'
+        
+    elif jogada_humana == 8 and matriz[2][1] == ' ':
+        matriz[2][1]= 'X'
+   
+    elif jogada_humana == 9 and matriz[2][2] == ' ':
+        matriz[2][2]= 'X'
+   
+    
+#define as jogadas da IA
+def jogadas_ia():
+    
+     print ("Jogada Inteligencia Artificial = ")
+     
+#verifica se alguem ganhou
+def verificar_ganhador():
+    
+    if matriz[0][0] == 'X' and matriz[0][1] == 'X' and matriz[0][2]=='X':
+        print("Temos um vencendor")
+        ganhador=1
+    if matriz[0][0] == 'O' and matriz[0][1] == 'O' and matriz[0][2]=='O':
+        print("Temos um vencendor")
+        ganhador=2
+
+    if matriz[0][0] == 'X' and matriz[1][0] == 'X' and matriz[2][0]=='X':
+        print("Temos um vencendor")
+        ganhador=1
+    if matriz[0][0] == 'O' and matriz[1][0] == 'O' and matriz[2][0]=='O':
+        print("Temos um vencendor")
+        ganhador=2
+
+    if matriz[0][0] == 'X' and matriz[1][1] == 'X' and matriz[2][2]=='X':
+        print("Temos um vencendor")
+        ganhador=1
+    if matriz[0][0] == 'O' and matriz[1][1] == 'O' and matriz[2][2]=='O':
+        print("Temos um vencendor")
+        ganhador=2
+    
+    
+
+#primeiro imprime o ambiente
+ambiente()
+ganhador=0
+verificar_ganhador()
+while ganhador == 0 :
+    verificar_ganhador()
+    jogadas_humana()
+    verificar_ganhador()
+    jogadas_ia()
+    verificar_ganhador()
+    os.system("cls")
+    if ganhador == 1:
+        break
+    else:
+        ambiente()
+        
+    
+
+
+
+
+
+
+
+
+
+
